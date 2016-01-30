c4a5cfd19a08a46348684fa84e1fb21e24c877f3
KibuzniK <malachi00@gmail.com>
2016-01-30 18:58:45 +0200

    WHaever

---
+++ Assets/FightHandler.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightHandler : MonoBehaviour {
+	public PlayerScript player;
+	public EnemyScript enemy;
+	private int numberOfButtons = 3;
+	char[] Buttons = {'n', 'm', 'p', 'v' };
+
+	public double guessTime = 5;
+	public bool isFighting = false;
+	private int tries;
+    private bool canTry = false;
+
+	private double currentGuessTime;
+	private char currentButton;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFighting && canTry) {
+            identifyButton();
+			if (currentGuessTime > 0) {
+				currentGuessTime -= Time.deltaTime;
+				if (currentGuessTime <= 0) {
+					FailGuess ();
+				}
+			}
+		}
+        //Debug.Log(currentGuessTime);
+	}
+
+    private void identifyButton()
+    {
+        if(Input.GetKeyDown(KeyCode.M)) {
+            Guess(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            Guess(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            Guess(3);
+        }
+    }
+
+    public void InitiateBattle(EnemyScript newEnemy, PlayerScript p)
+    {
+        if (isFighting == false)
+        {
+            isFighting = true;
+            player = p;
+            enemy = newEnemy;
+            enemy.isFighting = true;
+            enemy.Stop();
+            player.StartBattle();
+            //enemy.isEngaged = true;
+            tries = 3;
+            //Player.engage
+            StartTurn();
+        }
+	}
+
+    private void StartTurn()
+    {
+        
+        player.Runes.GetComponent<Animator>().SetInteger("Letter Index", 0);
+        /*
+        if (player.Runes.GetComponent<Animator>().GetInteger("Outcome") == -1)
+        {
+            player.Runes.GetComponent<Animator>().SetInteger("Outcome", 0);
+        }*/
+        canTry = true;
+        currentGuessTime = guessTime;
+        int buttonNum = RandomizeButton();
+        currentButton = Buttons[buttonNum];
+        DisplayButton(buttonNum);
+    }
+
+    public void Guess(int guessedButton)
+    {
+        char x = Buttons[guessedButton];
+        if (isFighting == true && currentGuessTime > 0)
+        {
+            canTry = false;
+            if (currentButton == x)
+            {
+                SucceedGuess();
+            }
+            else {
+                FailGuess();
+            }
+        }
+    }
+
+    private void FailGuess(){
+        canTry = false;
+		tries--;
+        player.FailPosses();
+        if (tries <= 0)
+        {
+            FailBattle();
+        }
+        else {
+            DislayFailedGuess();
+            StartTurn();
+        }
+	}
+
+	private void SucceedGuess(){
+        canTry = false;
+        if (enemy.Hit() == true)
+        {
+            WinBattle();
+        }
+        else {
+            DisplaySucceededGuess();
+            StartTurn();
+        }
+	}
+
+
+	private int RandomizeButton(){
+		int x = Random.Range (1, 4);
+        Debug.Log(x);
+		return x;
+	}
+
+	private void WinBattle(){
+        isFighting = false;
+        DisplayWonBattle(); // Perhaps halt is required
+        player.Posses(enemy);
+        // Playr. not fighting
+        //enemy.isEngaged = false;
+        //Player.Possess(foe)
+        ///
+    }
+
+    private void DisplayButton(int button){
+        player.Runes.GetComponent<Animator>().SetInteger("Letter Index", button);
+        ///
+    }
+
+    public void FailBattle(){
+        isFighting = false;
+        enemy.Free();
+        //player.FailPosses();
+        DisplayFailedBattle();
+        ///
+    }
+
+    private void DislayFailedGuess(){
+        player.Runes.GetComponent<Animator>().SetInteger("Outcome", -1);
+        //player.Runes.GetComponent<Animator>().SetInteger("Letter Index", 0);
+
+        ///
+    }
+
+    private void DisplaySucceededGuess (){
+        //player.Runes.GetComponent<Animator>().SetInteger("Outcome", 1);
+        player.Runes.GetComponent<Animator>().SetTrigger("Do");
+
+
+
+        ///
+    }
+
+    private void DisplayFailedBattle(){
+        player.Runes.GetComponent<Animator>().SetInteger("Letter Index", 0);
+        player.Runes.GetComponent<Animator>().SetInteger("Outcome", -1);
+
+
+        ///
+    }
+
+    private void DisplayWonBattle() {
+        player.Runes.GetComponent<Animator>().SetInteger("Letter Index", 0);
+        player.Runes.GetComponent<Animator>().SetInteger("Outcome", 0);
+        //player.Runes.GetComponent<Animator>().SetTrigger("Do");
+
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,55 +2,45 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-
-    [SerializeField] private Sprite Enemy1;
-    [SerializeField] private Animation Enemy1A;
-    [SerializeField] private Sprite Enemy2;
-    [SerializeField] private Animation Enemy2A;
-    [SerializeField] private Sprite Enemy3;
-    [SerializeField] private Animation Enemy3A;
-
     
     [SerializeField] float speed;
     static public int fightDelay;
-    [SerializeField] Animation[] fights;
-    [SerializeField] private Transform kathulu;
-    Transform mob;
+    [SerializeField] public Transform Runes;
+    [SerializeField] public Transform kathulu;
+    [SerializeField] public Transform Flame;
+    EnemyScript currentEnemy;
+
     Animator anim;
     enum Keys { K,P,R,C};
+    [SerializeField]
+    RuntimeAnimatorController enemy1Controller;
+    [SerializeField]
+    RuntimeAnimatorController enemy2Controller;
+    [SerializeField]
+    RuntimeAnimatorController enemy3Controller;
+    Animator animator;
+    [SerializeField]
+    public GameObject g;
+    private FightHandler fightHandler;
+    Light View;
 
     public float viewThreshold;
 	
 	void Start ()
     {
-       
-	}
-	
-	
-	void Update ()
+        View = GetComponentInChildren<Light>();
+        fightDelay = 1;
+        fightHandler = g.GetComponent<FightHandler>();
+
+    }
+
+
+    void Update ()
     {
         MovementControll();
         LightControl();
     }
-    void Posses(EnemyScript enemy)
-    {
-        GetComponent<SpriteRenderer>().sprite = enemy.GetComponent<SpriteRenderer>().sprite;
-        /* switch (enemy.type)
-         {
-             case 1:
-                 Debug.Log("AERGARG");
-                 GetComponent<SpriteRenderer>().sprite = enemy.GetComponent<SpriteRenderer>().sprite;
-                 break;
-             case 2:
-                 GetComponent<SpriteRenderer>().sprite = enemy.GetComponent<SpriteRenderer>().sprite;
-                 Debug.Log("AERGARG");
-                 break;
-             case 3:
-                 GetComponent<SpriteRenderer>().sprite = Enemy3;
-                 break;
-             default: break;
-         }*/
-    }
+    
     void MovementControll()
     {
         float localY = Input.GetAxis("Vertical");
@@ -84,31 +74,89 @@ public class PlayerScript : MonoBehaviour {
     }
     void OnTriggerStay2D(Collider2D col)
     {
-        if (col.CompareTag("Enemy")&&Input.GetButtonDown("Jump"))
-        {
-            Debug.Log("TROOO");
-            Posses(col.GetComponent<EnemyScript>());
+        if (col.CompareTag("Enemy")){
+            currentEnemy = col.GetComponent<EnemyScript>();
+            if (Input.GetButtonDown("Jump") && (!currentEnemy.isFighting))
+            {
+                Debug.Log("TROOO");
+                fightHandler.InitiateBattle(currentEnemy, this);
+            }
         }
     }
     void LightControl()
     {
-        Light View = GetComponentInChildren<Light>();
         View.range -= Time.deltaTime*viewThreshold;
         if(View.range<=3.5f)
         {
             View.range = 3.5f;
         }
+        else if (View.range >= 14){
+            View.range = 14;
+        }
     }
 
-    IEnumerator Fight()
+    /*
+    IEnumerator Fight(int type, RuntimeAnimatorController Eanim,EnemyScript enemy)
     {
-        yield return new WaitForSeconds(fightDelay);
-        yield return StartCoroutine(RandomButtonAction());
-    }
-    IEnumerator RandomButtonAction()
+        Debug.Log(enemy);
+        while (Flame.GetComponent<Animator>().GetInteger("Flame level")!=3&& Flame.GetComponent<Animator>().GetInteger("Flame level") != 0)
+        {
+            RandomButtonAction();
+            yield return null;
+        }
+        Victory(enemy.GetComponent<EnemyScript>());
+    }*/
+
+    /*
+    void RandomButtonAction()
     {
-        return null;
+        int buttonIndex = Random.Range(1, 3);
+        switch(buttonIndex)
+        {
+            case 1:
+                Runes.GetComponent<Animator>().SetInteger("Letter Index", buttonIndex);
+                StartCoroutine(WaitForKeyDown(KeyCode.M));
+                break;
+            case 2:
+                Runes.GetComponent<Animator>().SetInteger("Letter Index", buttonIndex);
+                StartCoroutine(WaitForKeyDown(KeyCode.P));
+                break;
+            case 3:
+                Runes.GetComponent<Animator>().SetInteger("Letter Index", buttonIndex);
+                StartCoroutine(WaitForKeyDown(KeyCode.V));
+                break;
+        }
+        
     }
+    */
+
+    public void StartBattle()
+    {
+    }
+
+    public void Posses(EnemyScript enemy)
+    {
+        View.range += 3;
+        animator = GetComponent<Animator>();
+        switch (enemy.type)
+        {
+            case 1:
+                animator.runtimeAnimatorController = enemy1Controller;
+                break;
+            case 2:
+                animator.runtimeAnimatorController = enemy2Controller;
+                break;
+            case 3:
+                animator.runtimeAnimatorController = enemy3Controller;
+                break;
+        }
+    }
+
+    public void FailPosses()
+    {
+        View.range -= 1;
+    }
+
     /*void Kathulu()
     {
         Instantiate(kathulu,new Vector2())
