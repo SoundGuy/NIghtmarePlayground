52bc9ce3661a61963e6424ad7eac432cdfffb2f0
kobi666 <kobi.malul@gmail.com>
2016-01-30 17:05:40 +0200

    cthulli amimation + script

---
+++ Assets/CthulliScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CthulliScript : MonoBehaviour {
+
+	public GameObject Player;
+	bool ChasePlayer;
+
+	void Start ()
+	{
+		this.transform.GetComponent<SpriteRenderer> ().enabled = false;
+		ChasePlayer = false;
+	}
+	public void activateCthulli()
+	{
+		ChasePlayer = true;
+	}
+	
+
+	void Update ()
+	{
+		if (ChasePlayer) 
+		{
+			this.transform.GetComponent<SpriteRenderer> ().enabled = true;
+			transform.position = Vector2.MoveTowards (transform.position, Player.transform.position, 1*Time.deltaTime);
+		}
+
+	}
+
+
+}
