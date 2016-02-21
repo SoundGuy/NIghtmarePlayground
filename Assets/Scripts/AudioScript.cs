d2fbc144e881b5540d4c41a26b644caba3cb497a
kobi666 <kobi.malul@gmail.com>
2016-02-21 21:16:42 +0200

    added stuff

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+
+	public bool freakout; 
+	public float VolumeSpeed;
+	public AudioClip audio1;
+	// Use this for initialization
+	void Start () {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.clip = audio1;
+		audio.Play ();
+
+
+	}
+
+
+
+	void FadeoutAudio () {
+		AudioSource audio = GetComponent<AudioSource>();
+
+
+			float VolumeDecrease = Time.deltaTime * VolumeSpeed;
+
+			audio.volume -= VolumeDecrease;
+
+	}
+
+	void freakStart () {
+	freakout = true;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ( freakout == true  && GetComponent<AudioSource>().volume != 0) 
+		{
+		FadeoutAudio ();
+			//Debug.Log ("im on");
+		}
+    }
+}
---
+++ Assets/tree_order.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tree_order : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		// If other is layer dependant
+		// If Im lower && other Layer greater - Accend layer order (SpriteRenderer>().sortingOrder +1)
+		if(other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy") 
+		{
+			//Debug.Log ("reached conditions");
+			if(GetMinYValue(other.gameObject) > GetMinYValue(this.gameObject) &&
+				other.gameObject.GetComponent<SpriteRenderer> ().sortingOrder >= gameObject.GetComponent<SpriteRenderer> ().sortingOrder) 
+			{
+				//Debug.Log(GetMinYValue(other.gameObject));
+				//Debug.Log (GetMinYValue (this.gameObject));
+				this.GetComponent<SpriteRenderer> ().sortingOrder += 1;
+			}
+		}
+	}
+
+	private float GetMinYValue(GameObject o){
+		float minY = o.GetComponent<SpriteRenderer> ().sprite.bounds.min.y;
+		float yLocation = o.transform.position.y + minY;
+		return yLocation;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
